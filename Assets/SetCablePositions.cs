9f562fecf99cc5069a8cb2a9b4298db24679a667
William Pheloung <wpheloung@gmail.com>
2018-01-27 09:25:54 -0800

    phone lines

---
+++ Assets/SetCablePositions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCablePositions : MonoBehaviour {
+    [SerializeField]
+    private GameObject m_cableA;
+    [SerializeField]
+    private GameObject m_cableB;
+
+    [SerializeField]
+    private LineRenderer m_cableRenderer;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_cableRenderer.positionCount = 2;
+        m_cableRenderer.SetPosition(0, m_cableA.transform.position);
+        m_cableRenderer.SetPosition(1, m_cableB.transform.position);
+    }
+}
