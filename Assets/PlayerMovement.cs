23803a4f9b6d797e0e8fde72e77afc7a4f777627
William Pheloung <wpheloung@gmail.com>
2018-01-28 12:38:41 -0800

    whatever

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -19,6 +19,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private AudioSource _audioclip;
 
+    [SerializeField]
+    private ParticleSystem _emitter;
+
     void Awake()
     {
     }
@@ -27,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
         {
+            _emitter.Emit(100);
             _audioclip.PlayOneShot(_audioclips[Random.Range(0, _audioclips.Count)]);
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             m_rigidbody.AddForce((new Vector2(worldPos.x, worldPos.y) - new Vector2(transform.position.x, transform.position.y)).normalized * c_movementConstant);
